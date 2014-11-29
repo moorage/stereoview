ccc88e3a85ca9c0869046834d7097233029974eb
Matthew Moore <m3moore@gmail.com>
2014-11-29 12:10:32 -0800

    show axes

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,7 +1,7 @@
 var defaultPointSize = 0.04;
 var defaultLOD = 10000;
 var pointcloudPath = "/js/scene/cloud.js";
-// var pointcloudPath = "/js/scene/out1.ply"; // potree can't read .ply files =[
+// var pointcloudPath = "/js/scene/out1.ply"; // potree can't read .ply files 
    
 
 
@@ -11,9 +11,43 @@ var scene;
 var pointcloud;
 var pointcloudMaterial;
 
+function buildAxis( src, dst, colorHex, dashed ) {
+  var geom = new THREE.Geometry(),
+    mat; 
 
+  if(dashed) {
+    mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+  } else {
+    mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+  }
+
+  geom.vertices.push( src.clone() );
+  geom.vertices.push( dst.clone() );
+  geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+
+  var axis = new THREE.Line( geom, mat, THREE.LinePieces );
+  return axis;
+}
+
+function buildAxes( length ) {
+  var axes = new THREE.Object3D();
+
+  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xFF0000, false ) ); // +X
+  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), 0xFF0000, true) ); // -X
+  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0x00FF00, false ) ); // +Y
+  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), 0x00FF00, true ) ); // -Y
+  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0x0000FF, false ) ); // +Z
+  axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), 0x0000FF, true ) ); // -Z
+
+  return axes;
+
+}
+  
 function init(){
   scene = new THREE.Scene();
+	axes = buildAxes( 1000 );
+	scene.add( axes );
+  
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100000);
 
   renderer = new THREE.WebGLRenderer();
